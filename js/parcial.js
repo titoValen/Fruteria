467f18a12114574c0564a2da4a9f3a196a16be92
titoValen <valentin.tito@davinci.edu.ar>
2025-11-10 14:43:52 -0300

    Agregar archivos iniciales: HTML, CSS, JS y favicon para la aplicación de carrito de compras

---
+++ js/parcial.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*
+ *	APELLIDO, NOMBRE
+ */
+
+let aProductos = [
+	{
+		nombre: 'Nombre del producto 1',
+		imagen: 'producto-de-ejemplo.jpg',
+		descripcion: 'Descripción del producto 1',
+		precio: 100,
+	},
+	{
+		nombre: 'Nombre del producto 2',
+		imagen: 'producto-de-ejemplo.jpg',
+		descripcion: 'Descripción del producto 2',
+		precio: 200,
+	},
+	{
+		nombre: 'Nombre del producto 3',
+		imagen: 'producto-de-ejemplo.jpg',
+		descripcion: 'Descripción del producto 3',
+		precio: 300,
+	},
+	{
+		nombre: 'Nombre del producto 4',
+		imagen: 'producto-de-ejemplo.jpg',
+		descripcion: 'Descripción del producto 4',
+		precio: 400,
+	},
+	{
+		nombre: 'Nombre del producto 5',
+		imagen: 'producto-de-ejemplo.jpg',
+		descripcion: 'Descripción del producto 5',
+		precio: 500,
+	},
+	{
+		nombre: 'Nombre del producto 6',
+		imagen: 'producto-de-ejemplo.jpg',
+		descripcion: 'Descripción del producto 6',
+		precio: 600,
+	},
+];
